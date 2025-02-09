8800db0e7d771df0cace689c2f12dc08282f6ecb
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-09 09:24:57 +0300

    storage

---
+++ attendance/components/storage.js
@@ -0,0 +1,22 @@
+// components/storage.js
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// Save data
+export const saveData = async (key, value) => {
+  try {
+    await AsyncStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.error('Error saving data:', error);
+  }
+};
+
+// Retrieve data
+export const getData = async (key) => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+  } catch (error) {
+    console.error('Error retrieving data:', error);
+    return null;
+  }
+};
