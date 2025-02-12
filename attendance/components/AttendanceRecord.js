edf2fcd1ae28ef2c7afa1b268de461a0f7452358
yohannes yeneakal teshome <yohannesyeneakal1@gmail.com>
2025-02-12 20:21:32 +0300

    attendance recored background

--- attendance/components/AttendanceRecord.js
+++ attendance/components/AttendanceRecord.js
@@ -59,6 +59,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     borderWidth: 1,
     borderColor: '#ccc',
+    backgroundColor: 'transparent',
   },
   tableHeader: {
     flexDirection: 'row',
